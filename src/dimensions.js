9a58c986850bb9028da5af1a71027c93a0ebafed
gungorn <gungorn@outlook.com>
2021-01-03 19:07:46 +0300

    export ++

--- src/dimensions.js
+++ src/dimensions.js
@@ -1,20 +1,20 @@
 import { Dimensions, Platform } from 'react-native';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
 
-const window = Dimensions.get('window');
-const screen = Dimensions.get('screen');
+export const window = Dimensions.get('window');
+export const screen = Dimensions.get('screen');
 
-const w = window.width;
-const h = window.height;
+export const w = window.width;
+export const h = window.height;
 
-const W = d => w * d / 100;
-const H = d => h * d / 100;
+export const W = d => w * d / 100;
+export const H = d => h * d / 100;
 
-const sbh = getStatusBarHeight(); //ios: durum çubuğu yükseliği, android: durum çubuğu yüksekliği
-const sbhi = getStatusBarHeight(true); //ios: durum çubuğu yükseliği, android: 0
+export const sbh = getStatusBarHeight(); //ios: durum çubuğu yükseliği, android: durum çubuğu yüksekliği
+export const sbhi = getStatusBarHeight(true); //ios: durum çubuğu yükseliği, android: 0
 
-const OS = Platform.OS;
-const IOS = OS === 'ios';
-const AND = OS === 'android';
+export const OS = Platform.OS;
+export const IOS = OS === 'ios';
+export const AND = OS === 'android';
 
-export { window, screen, w, h, W, H, sbh, sbhi, OS, IOS, AND };
+//export { window, screen, w, h, W, H, sbh, sbhi, OS, IOS, AND };
